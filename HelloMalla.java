1ccd3c81e3740925619b91fe65b793f74d23b791
mallareddy63 <mallareddy63@gmail.com>
2021-11-19 17:07:45 +0530

    Update HelloMalla.java

--- HelloMalla.java
+++ HelloMalla.java
@@ -1,6 +1,6 @@
-public class HelloWorld {
+public class HelloMalla {
  public static void main(String[] args) {
-   //Prints "hello, world" in terminal window.
-     System.out.println("Hello, World");
+   //Prints "hello, malla" in terminal window.
+     System.out.println("Hello, Malla");
  }
 }
